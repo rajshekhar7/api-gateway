aacdcd52168570df163db80c1873349af062127f
Raj Shekhar <rajshekharpcs@gmail.com>
2020-04-26 23:23:00 +0530

    added all the files

---
+++ go.mod
@@ -0,0 +1,14 @@
+module main
+
+go 1.14
+
+require (
+	github.com/jinzhu/gorm v1.9.12
+	github.com/joho/godotenv v1.3.0
+	github.com/mattn/go-sqlite3 v2.0.3+incompatible // indirect
+	github.com/tidwall/buntdb v1.1.2 // indirect
+	github.com/tidwall/gjson v1.6.0 // indirect
+	github.com/tidwall/pretty v1.0.1 // indirect
+	golang.org/x/crypto v0.0.0-20200423211502-4bdfaf469ed5
+	gopkg.in/oauth2.v3 v3.12.0
+)
---
+++ main.go
@@ -0,0 +1,197 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"github.com/joho/godotenv"
+	"golang.org/x/crypto/bcrypt"
+	"gopkg.in/oauth2.v3"
+	"gopkg.in/oauth2.v3/errors"
+	"gopkg.in/oauth2.v3/manage"
+	"gopkg.in/oauth2.v3/models"
+	"gopkg.in/oauth2.v3/server"
+	"gopkg.in/oauth2.v3/store"
+)
+
+var db *gorm.DB
+
+// var router *mux.Router
+
+type User struct {
+	ID       string `gorm:"primary_key;not null;unique" json:"id"`
+	Username string `gorm:"size:255;not null;unique" json:"username"`
+	Email    string `gorm:"size:100;not null;unique" json:"email"`
+	Password string `gorm:"size:100;not null;" json:"password"`
+}
+
+func Hash(password string) ([]byte, error) {
+	return bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+}
+
+func VerifyPassword(hashedPassword, password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
+
+func (u *User) BeforeSave() error {
+	hashedPassword, err := Hash(u.Password)
+	if err != nil {
+		return err
+	}
+	u.Password = string(hashedPassword)
+	return nil
+}
+
+func init() {
+	// loads values from .env into the system
+	if err := godotenv.Load(); err != nil {
+		log.Print("sad .env file found")
+	}
+}
+
+func main() {
+
+	bytes, err := ioutil.ReadFile("seedUsers.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Decode
+	var users []User
+	if err := json.Unmarshal(bytes, &users); err != nil {
+		log.Fatal(err)
+	}
+
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error getting env, %v", err)
+	} else {
+		fmt.Println("We are getting the env values")
+	}
+
+	//reading database
+	dbDriver := os.Getenv("DB_DRIVER")
+	dbName := os.Getenv("DB_NAME")
+	db, err = gorm.Open(dbDriver, dbName)
+	if err != nil {
+		fmt.Printf("Cannot connect to %s database\n", dbDriver)
+		log.Fatal("This is the error:", err)
+	} else {
+		fmt.Printf("We are connected to the %s database\n", dbDriver)
+	}
+	db.Exec("PRAGMA foreign_keys = ON")
+
+	err = db.Debug().DropTableIfExists(&User{}).Error
+	if err != nil {
+		log.Fatalf("cannot drop table: %v", err)
+	}
+	err = db.Debug().AutoMigrate(&User{}).Error
+	if err != nil {
+		log.Fatalf("cannot migrate table: %v", err)
+	}
+
+	for i, _ := range users {
+		// users[i].Password = hash
+		err = db.Debug().Model(&User{}).Create(&users[i]).Error
+		if err != nil {
+			log.Fatalf("cannot seed users table: %v", err)
+		}
+		log.Println(users[i].ID, users[i].Username, users[i].Email, users[i].Password)
+	}
+
+	//oauth
+
+	manager := manage.NewDefaultManager()
+
+	cfg := &manage.Config{
+		AccessTokenExp:    time.Minute * 5,
+		RefreshTokenExp:   time.Hour * 24 * 7,
+		IsGenerateRefresh: false,
+	}
+	manager.SetPasswordTokenCfg(cfg)
+
+	manager.MustTokenStorage(store.NewMemoryTokenStore())
+
+	clientStore := store.NewClientStore()
+
+	AppID := os.Getenv("APP_ID")
+	AppSecret := os.Getenv("APP_SECRET")
+
+	for _, p := range users {
+		clientStore.Set(AppID, &models.Client{ID: p.ID, Secret: AppSecret})
+	}
+
+	manager.MapClientStorage(clientStore)
+
+	srv := server.NewDefaultServer(manager)
+	srv.SetAllowGetAccessRequest(true)
+	srv.SetAllowedGrantType(oauth2.PasswordCredentials)
+
+	srv.SetClientInfoHandler(server.ClientFormHandler)
+
+	srv.SetPasswordAuthorizationHandler(func(username, password string) (userID string, err error) {
+		userID = ""
+		user := User{}
+		err = db.Debug().Model(User{}).Where("email = ?", username).Take(&user).Error
+		if err != nil {
+			return "", err
+		}
+		err = VerifyPassword(user.Password, password)
+		if err != nil && err == bcrypt.ErrMismatchedHashAndPassword {
+			return "", err
+		}
+		userID = user.ID
+		return
+	})
+
+	srv.SetInternalErrorHandler(func(err error) (re *errors.Response) {
+		log.Println("Internal Error:", err.Error())
+		return
+	})
+
+	srv.SetResponseErrorHandler(func(re *errors.Response) {
+		log.Println("Response Error:", re.Error.Error())
+	})
+
+	//oauth
+	// router = mux.NewRouter()
+
+	http.HandleFunc("/oauth", func(w http.ResponseWriter, r *http.Request) {
+		srv.HandleTokenRequest(w, r)
+	})
+
+	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
+		token, err := srv.ValidationBearerToken(r)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		user := User{}
+		db.Debug().Model(User{}).Where("id = ?", token.GetUserID()).Take(&user)
+
+		// data := map[string]interface{}{
+		// 	"expires_in": int64(token.GetAccessCreateAt().Add(token.GetAccessExpiresIn()).Sub(time.Now()).Seconds()),
+		// 	"client_id":  token.GetClientID(),
+		// 	"user_id":    token.GetUserID(),
+		// }
+
+		e := json.NewEncoder(w)
+		e.SetIndent("", "  ")
+		e.Encode(user)
+
+	})
+
+	// router.HandleFunc("/oauth", )
+	// router.HandleFunc("/home")
+
+	fmt.Println("Listening to port 8000")
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
